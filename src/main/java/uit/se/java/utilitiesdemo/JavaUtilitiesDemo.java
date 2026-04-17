e2bf38439b09e82582f941c072fcad020ccbdea1
hntin <tin.huynh.uit@gmail.com>
2026-04-17 15:37:37 +0700

    update

--- src/main/java/uit/se/java/utilitiesdemo/JavaUtilitiesDemo.java
+++ src/main/java/uit/se/java/utilitiesdemo/JavaUtilitiesDemo.java
@@ -1,19 +1,41 @@
 package uit.se.java.utilitiesdemo;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class JavaUtilitiesDemo {
 
     public static void main(String[] args) {
         System.out.println("JavaUtilitiesDemo");
-
+        JavaUtilitiesDemo demo = new JavaUtilitiesDemo();
         String password = "aBc123456";
+        demo.isValidPass(password);
+        demo.setDemo2();
+    }
 
-        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,}$";
+    public void setDemo1() {
+        Set<String> set = new HashSet<>();
+        set.add("Java");
+        set.add("Java"); // bị loại
+        System.out.println(set);
+    }
 
-        if (password.matches(regex)) {
-            System.out.println("Mật khẩu hợp lệ");
-        } else {
-            System.out.println("Mật khẩu không hợp lệ");
-        }
+    public void setDemo2() {
+        String text = "java java collection set set java util";
+        String[] words = text.split(" ");
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
+        System.out.println(uniqueWords);
 
     }
+
+    public boolean isValidPass(String password) {
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,}$";
+        if (password.matches(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
